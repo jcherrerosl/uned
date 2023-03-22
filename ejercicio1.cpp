1573c8600e7b6cc203ceda128a6619e2296483e1
Juan Carlos <121610967+jcherrerosl@users.noreply.github.com>
2023-03-22 20:12:20 +0100

    Añadida función fun_upper();

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -3,38 +3,45 @@
 
 using namespace std;
 
-int main() {
-	double	temp;
-	char	unit;
+static char    fun_upper(char c){
+    if (c >= 'a' && c <='z')
+        c = c - 32;
+    return c;
+}
 
-	cout << "Escriba temperatura con unidades: ";
-	cin >> temp >> unit;
-	
-	double celsius, fahrenheit, kelvin;
-	
-	switch (unit) {
-		case 'C':
-			celsius = temp;
-			fahrenheit = celsius * 1.8 + 32;
-			kelvin = celsius + 273.15;
-			break;
-		case 'F':
-			fahrenheit = temp;
-			celsius = (fahrenheit - 32) / 1.8;
-			kelvin = celsius + 273.15;
-			break;
-		case 'K':
-			kelvin = temp;
-			celsius = kelvin - 273.15;
-			fahrenheit = celsius * 1.8 + 32;
-			break;
-		default:
-			cerr << "Unidad de temperatura inválida. Debe ser C, F o K." << endl;
-			return 1;
-	}
-	
-	cout << fixed << setprecision(2) << celsius << " C, " << fahrenheit << " F, " << kelvin << " K." << endl;
-	
-	return 0;
+int main() {
+    double  temp;
+    char    unit;
+
+    cout << "Escriba temperatura con unidades: ";
+    cin >> temp >> unit;
+    unit = fun_upper(unit);
+    
+    double celsius, fahrenheit, kelvin;
+    
+    switch (unit) {
+        case 'C':
+            celsius = temp;
+            fahrenheit = celsius * 1.8 + 32;
+            kelvin = celsius + 273.15;
+            break;
+        case 'F':
+            fahrenheit = temp;
+            celsius = (fahrenheit - 32) / 1.8;
+            kelvin = celsius + 273.15;
+            break;
+        case 'K':
+            kelvin = temp;
+            celsius = kelvin - 273.15;
+            fahrenheit = celsius * 1.8 + 32;
+            break;
+        default:
+            cerr << "Unidad de temperatura inválida. Debe ser C, F o K." << endl;
+            return 1;
+    }
+    
+    cout << fixed << setprecision(2) << celsius << " C, " << fahrenheit << " F, " << kelvin << " K." << endl;
+    
+    return 0;
 }
 
